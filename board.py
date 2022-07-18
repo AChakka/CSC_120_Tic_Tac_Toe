0a407d5f682974ce2f4fb5ced5f66a5121bc2c1e
AChakka <109451075+AChakka@users.noreply.github.com>
2022-07-18 03:27:08 -0400

    Add files via upload

---
+++ board.py
@@ -0,0 +1,12 @@
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+def print_board():
+    print("\n")
+    print(board[0] + " | " + board[1] + " | " + board[2]  )
+    print(board[3] + " | " + board[4] + " | " + board[5]  )
+    print(board[6] + " | " + board[7] + " | " + board[8]  )
+    print("\n")
+
+print_board()
